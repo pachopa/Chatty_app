bad4bbdc34da9a64ec497e1cb9c504d58396c747
chris <pachopa0320@gmail.com>
2017-06-27 21:23:09 +0000

    first commit and create Chatbar.jsx, Message.jsx, MessageList.jsx

---
+++ src/ChatBar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ChatBar extends Component {
+  render() {
+    return (
+      <footer class="chatbar">
+        <input class="chatbar-username" placeholder="Your Name (Optional)" />
+        <input class="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+
+export default ChatBar;
