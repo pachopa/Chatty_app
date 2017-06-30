03c489ad442e1fe74d3d6fe9989adc6f7c85a2ce
chris <pachopa0320@gmail.com>
2017-06-30 19:32:09 +0000

    insert switch statement to distinguish between incomingNotification and message

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,10 +4,19 @@ class Message extends Component {
   render() {
     return (
       <div>
-        <div className="message">
-          <span className="message-username">{this.props.Name}</span>
-          <span className="message-content">{this.props.Message}</span>
-        </div>
+        {(() => {
+          switch (this.props.type) {
+            case "incomingNotification":   
+              return <div className="message system">
+                      <span>{this.props.Message}</span>
+                     </div>;
+            default:      
+              return <div className="message">
+                        <span className="message-username">{this.props.Name}</span>
+                        <span className="message-content">{this.props.Message}</span>
+                      </div>;
+          }
+        })()}
       </div>
     );
   }
