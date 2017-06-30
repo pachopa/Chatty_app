37cd6a207db894d70313b0429a2ae5b5ed7febcf
chris <pachopa0320@gmail.com>
2017-06-30 19:30:46 +0000

    create newUserName function to update new name

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
-  
+  chatBarMessage(event) {
+    //send the message when we press the enter
+    if(event.key === 'Enter') {
+      this.props.newMessage(event.target.value);
+      event.target.value = "";
+    }
+  }
+
+  newUserName(event) {
+    if(event.key === 'Enter') {
+      console.log(event.target.value, "chatbar.jsx")
+      this.props.updateUserName(event.target.value);
+    } 
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.userName} />
-        <input onKeyUp={this.props.newMessage} className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" onKeyUp={this.newUserName.bind(this)} defaultValue={this.props.userName} />
+        <input id="chatBarMessage" onKeyUp={this.chatBarMessage.bind(this)} className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
