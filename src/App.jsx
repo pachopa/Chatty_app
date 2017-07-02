b2161ece19192ae03c224ef2dd137e2ffaae882e
chris <pachopa0320@gmail.com>
2017-07-02 17:55:14 +0000

    make an this.state.id = [] for saving the user id and count to send it to the server for each user color

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
- 
   constructor(props) {
     super(props);
     this.socket = this.createSocketFunction()
     this.state = {
       currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [],
-      usercount: 0
+      usercount: 0,
+      styles: "",
+      id: []
     }
     this.newMessage = this.newMessage.bind(this);
     this.updateUserName = this.updateUserName.bind(this);
   }
 
   createSocketFunction() {
+    //connet with web socket and take information from server
     let socket = new WebSocket('ws://localhost:3001/');
     socket.onmessage = (event) => {
       const newMessage = JSON.parse(event.data);
@@ -27,13 +29,20 @@ class App extends Component {
           this.setState({messages: messages});
           break;
         case "incomingNotification":
+          //handle incomingNotification
           var messages = this.state.messages.concat(newMessage)
           this.setState({messages: messages});
-          // handle incoming notification
+          var msgLength = messages.length;
           break;
         case 'incomingUser':
+          //handle incomingUser
+          const userInformation = {"id": newMessage.id, "count": newMessage.count}
           this.setState({usercount: newMessage.count});
-          // handle incoming notification
+          if(this.state.id.length < newMessage.count) {
+            this.setState(() => {
+              this.state.id.push(userInformation);
+            });
+          }
           break;
         default:
           // show an error in the console if the message type is unknown
@@ -47,13 +56,14 @@ class App extends Component {
     //update User Name and give a postNotification to the server
     const previousUserName = this.state.currentUser.name;
     this.setState({currentUser: {name: name}});
-    const newUserName = { username: name, type: 'postNotification', content: `${previousUserName} has changed their name to ${name}` }
+    const newUserName = {  username: name, type: 'postNotification', content: `${previousUserName} has changed their name to ${name}` }
     this.socket.send(JSON.stringify(newUserName));
   }
 
   newMessage(content) {
     // Add a new message to the list of messages in the data store
-    const newMessage = {type: 'postMessage', username: this.state.currentUser.name, content: content};
+    console.log("newMessage function: ", this.state.id)
+    const newMessage = {id: this.state.id, type: 'postMessage', username: this.state.currentUser.name, content: content};
     this.socket.send(JSON.stringify(newMessage));
   }
 
