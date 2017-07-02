fa86163e26af0844f1bff41735d5b2a7c92b1b88
chris <pachopa0320@gmail.com>
2017-07-02 17:51:32 +0000

    create random generate function for font color and save the user information with the color which is given each connect user

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,4 @@
 // server.js
-
 const express = require('express');
 const SocketServer = require('ws').Server;
 const WebSocket = require('ws');
@@ -15,6 +14,10 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 const uuidv1 = require('uuid/v1');
+
+//user Information
+const userInformation = {};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
@@ -28,33 +31,45 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
+wss.randomColorFunction = function randomColorFunction() {
+  //generate random font color
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  const cssRandomColor = '#' + randomColor;
+  return cssRandomColor;
+}
+
 wss.countUser = function countUser(data) {
   //counting for users how many people are connected.
   const usersNum = {
     type : 'incomingUser',
-    count : data
+    count : data,
+    id : uuidv1(),
+    styles: wss.randomColorFunction()
   }
+  userInformation[usersNum.id] = {"id": usersNum.id, "styles": usersNum.styles, "count": usersNum.count}
   return usersNum;
 }
 
 wss.on('connection', (ws) => {
   //connet between server and client side
-  var userCount = wss.countUser(wss.clients.size);
+  const userCount = wss.countUser(wss.clients.size);
   wss.broadcast(JSON.stringify(userCount)); 
-
+  
   ws.on('message', (messages) => {
-    var Msg = JSON.parse(messages);
+    const Msg = JSON.parse(messages);
     if(Msg.type === 'postNotification') {
-      Msg.id = uuidv1();
       Msg.type = 'incomingNotification'
-      wss.broadcast(JSON.stringify(Msg)); 
+      wss.broadcast(JSON.stringify(Msg));
     } 
     if(Msg.type === 'postMessage'){
-      Msg.id = uuidv1();
+      for(var userid in userInformation) {
+        if(userInformation[userid].count === Msg.id[0].count) {
+          Msg.styles = userInformation[userid].styles;
+        }
+      }
       Msg.type = 'incomingMessage'
       wss.broadcast(JSON.stringify(Msg)); 
     }
-   
   })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
